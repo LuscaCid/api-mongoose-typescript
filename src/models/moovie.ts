8a8d1f99e00895c03dd55a8fcbb1d21acdc59495
Lucas Cid <lucasfelipaaa@gmail.com>
2024-01-13 09:42:04 -0300

    comment at moovie.ts

--- src/models/moovie.ts
+++ src/models/moovie.ts
@@ -11,3 +11,9 @@ const MoovieSchema : Schema = new Schema({
 })
 
 export const MoovieModel = model("Movie",MoovieSchema)
+
+/**
+ * futuramente irei criar uma tabela para usuarios da application e para ter acesso aos comentarios sobre os filmes
+ * 
+ * 
+ */
